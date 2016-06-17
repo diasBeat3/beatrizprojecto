95c5936668d4695dbac376803724cde4a8bf19f9
diasBeat3 <dias.anabeatriz3@gmail.com>
2016-06-17 15:17:24 +0100

    firstcommit

---
+++ assets/js/custom.js
@@ -0,0 +1,63 @@
+jQuery(document).ready(function($) { 
+    $(".scroll").click(function(event){        
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+   });
+});
+
+
+$(function(){   
+			var nav = $('#menuf');   
+			$(window).scroll(function () { 
+				if ($(this).scrollTop() > 900) { 
+					nav.addClass("menu-fixo"); 
+				} else { 
+					nav.removeClass("menu-fixo"); 
+				} 
+			});  
+		});
+
+
+$(document).ready(function(){
+	
+	$(".thumbs a").click(function() {
+		var path = $(this).attr("href");
+		var alt = $(this).attr("title");
+		
+		$("#imagem").attr({src: path, alt: alt});
+		return false;
+	});	
+});
+
+$(document).ready(function(){
+    $("a[rel=modal]").click( function(ev){
+        ev.preventDefault();
+ 
+        var id = $(this).attr("href");
+ 
+        var alturaTela = $(document).height();
+        var larguraTela = $(window).width();
+     
+        //colocando o fundo preto
+        $('#mascara').css({'width':larguraTela,'height':alturaTela});
+        $('#mascara').fadeIn(1000); 
+        $('#mascara').fadeTo("slow",0.8);
+ 
+        var left = ($(window).width() /2) - ( $(id).width() / 2 );
+        var top = ($(window).height() / 2) - ( $(id).height() / 2 );
+     
+        $(id).css({'top':top,'left':left});
+        $(id).show();   
+    });
+ 
+    $("#mascara").click( function(){
+        $(this).hide();
+        $(".window").hide();
+    });
+ 
+    $('.fechar').click(function(ev){
+        ev.preventDefault();
+        $("#mascara").hide();
+        $(".window").hide();
+    });
+});
